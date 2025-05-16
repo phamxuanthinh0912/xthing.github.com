ffa82057a1a2af118041ebb235da6c05be8c3dd9
phamxuanthinh0912 <nhuoclinh99@gmail.com>
2025-05-16 17:41:05 +0700

    done

--- Function.js
+++ Function.js
@@ -62,11 +62,5 @@ elements.forEach(el => observer.observe(el));
   });
 
 // top selling
- const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+
+
